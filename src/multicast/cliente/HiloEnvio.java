6c696857ff4c28c35250797d908d51145b42fe74
dapelle <dapelle@Saturno>
2016-03-17 15:25:50 +0100

    Nuevo mecanismo de empaquetado y envio
    - Modificado la forma de empaquetar ahora empaqueta como tar todos los directorios
    - Modificado HiloEnvio para enviar un tar como binario (mas sencillo)
    - Eliminados archivos redundantes HiloCompresor / HiloCompresorHijo

--- src/multicast/cliente/HiloEnvio.java
+++ src/multicast/cliente/HiloEnvio.java
@@ -23,19 +23,19 @@ import multicast.lib.Log;
  */
 public class HiloEnvio extends Thread {
 
-    private LinkedList<String> listaComprimidos;
     private MulticastSocket enviador;
     public static final String INET_ADDR = "224.0.0.3";
     public static final int PORT = 8888;
     private InetAddress addr;
     private DatagramSocket serverSocket;
     private Log log;
-    
-    public HiloEnvio(LinkedList<String> listaComprimidos, Log log) {
+    private String tarPath;
+
+    public HiloEnvio(String tarPath, Log log) {
         try {
-            this.listaComprimidos = listaComprimidos; // Ficheros compridos
+            this.tarPath = tarPath; // Ficheros compridos
             // Nos creamos la conexion
-            addr = InetAddress.getByName(INET_ADDR);         
+            addr = InetAddress.getByName(INET_ADDR);
             serverSocket = new DatagramSocket();
             this.log = log;
         } catch (UnknownHostException | SocketException ex) {
@@ -45,44 +45,25 @@ public class HiloEnvio extends Thread {
 
     @Override
     public void run() {
-        String ficheroAEnviar;
-
-        while (true) {
-            try {                
-                if (!listaComprimidos.isEmpty()) {
-                    ficheroAEnviar = listaComprimidos.removeFirst();
-                    enviarFichero(ficheroAEnviar);
-                    log.añadir("Enviado " + ficheroAEnviar);
-                } else {
-                    sleep(500);
-                }                    
-            } catch (Exception ex) {
-                Logger.getLogger(HiloEnvio.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-
-
-    private void enviarFichero(String ficheroAEnviar) {
         int leidos;
         byte[] datos = new byte[4096];
         DatagramPacket msgPacket;
-        byte fin[] = new byte[] {-1};
-        
-        try {            
+        byte fin[] = new byte[]{-1};
+
+        try {
             // Se abre el fichero original para lectura            
-            FileInputStream fileInput = new FileInputStream(ficheroAEnviar);                     
+            FileInputStream fileInput = new FileInputStream(this.tarPath);
             BufferedInputStream bufferedInput = new BufferedInputStream(fileInput);
-                                   
+
             leidos = bufferedInput.read(datos);
             while (leidos > 0) {
-                msgPacket = new DatagramPacket(datos,datos.length, addr, PORT);                
-                serverSocket.send(msgPacket);                
-                leidos=bufferedInput.read(datos);
+                msgPacket = new DatagramPacket(datos, datos.length, addr, PORT);
+                serverSocket.send(msgPacket);
+                leidos = bufferedInput.read(datos);
             }
             //  Aviso de fin de fichero
-            msgPacket = new DatagramPacket(fin,1, addr, PORT);
-            serverSocket.send(msgPacket);  
+            msgPacket = new DatagramPacket(fin, 1, addr, PORT);
+            serverSocket.send(msgPacket);
             // Cierre de los ficheros
             bufferedInput.close();
         } catch (UnknownHostException ex) {
@@ -92,7 +73,6 @@ public class HiloEnvio extends Thread {
         } catch (IOException ex) {
             Logger.getLogger(HiloEnvio.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
 }
--- src/multicast/cliente/HiloPrincipal.java
+++ src/multicast/cliente/HiloPrincipal.java
@@ -5,38 +5,122 @@
  */
 package multicast.cliente;
 
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
 import multicast.cliente.gui.JFrameMain;
-import multicast.lib.Log;
+import multicast.lib.*;
+import org.xeustechnologies.jtar.*;
 
 /**
  *
  * @author PROF_VESPERTINO
  */
-public class HiloPrincipal extends Thread{
-    private Thread hiloCompresor;
-    private Thread hiloEnvio;    
+public class HiloPrincipal extends Thread {
+
+    private Thread hiloEnvio;
     private LinkedList<String> listaSelecionados;
-    private LinkedList<String> listaComprimidos;
     private JFrameMain ventana;
     private Log log;
+    String tarPath;
 
     public HiloPrincipal(LinkedList<String> listaSelecionados, JFrameMain ventana) {
         this.listaSelecionados = listaSelecionados;
-        listaComprimidos = new LinkedList<String>();    
         log = new Log(ventana.getJTextAreaLog());
     }
-       
-    
+
     @Override
-    public void run() {                
-        // Arrancamos hilos
-        hiloCompresor = new HiloCompresor(listaSelecionados, listaComprimidos, log);        
-        hiloEnvio = new HiloEnvio(listaComprimidos, log);        
-        hiloCompresor.start();
-        hiloEnvio.start();
+    public void run() {
+        log.añadir("Preparando paquete de envio...");
+        empaquetar_directorios();
+        log.añadir("Paquete preparado!!!");
+        log.añadir("Enviando paquete a los clientes...");
+        Thread he = new HiloEnvio(tarPath, log);   
+        he.start();
         
-        
+
     }
-    
+
+    private void empaquetar_directorios() {
+        FileOutputStream fOut = null;
+        TarOutputStream out = null;
+        int i = 0;
+        String next;
+
+        try {
+            // Output file stream
+            tarPath = "PAQUETE.tar";
+            fOut = new FileOutputStream(new File(tarPath));
+            // Create a TarOutputStream
+            out = new TarOutputStream(new BufferedOutputStream(fOut));
+            // Files to tar            
+            for (Iterator<String> iterator = listaSelecionados.iterator(); iterator.hasNext();) {
+                String dirPath = iterator.next();
+
+                System.out.println(new File(".").getAbsolutePath());
+                addFileToTar(out, dirPath, "");
+
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(HiloPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                out.close();
+                fOut.close();
+            } catch (IOException ex) {
+                Logger.getLogger(HiloPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void addFileToTar(TarOutputStream tOut, String path, String base) {
+        File f = new File(path);
+        String entryName = base + f.getName();
+
+        try {
+            TarEntry tarEntry = new TarEntry(f, entryName);
+            tOut.putNextEntry(tarEntry);
+
+            if (f.isFile()) {
+                copiarFicheros(new FileInputStream(f), tOut);                
+
+            } else {                
+                File[] children = f.listFiles();
+                if (children != null) {
+                    for (File child : children) {
+                        log.añadir(entryName + " añadido al paquete de envio");
+                        addFileToTar(tOut, child.getAbsolutePath(), entryName + "/");
+                    }
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(HiloPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(HiloPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void copiarFicheros(FileInputStream fileInputStream, TarOutputStream tOut) {
+        BufferedInputStream origin = null;
+        try {
+            origin = new BufferedInputStream(fileInputStream);
+
+            int count;
+            byte data[] = new byte[2048];
+            while ((count = origin.read(data)) != -1) {
+                tOut.write(data, 0, count);
+            }
+
+            tOut.flush();
+            origin.close();
+        } catch (IOException ex) {
+            Logger.getLogger(HiloPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void enviarPaquete() {
+        System.out.println("Enviar paquete no implementado aun");
+    }
+
 }
